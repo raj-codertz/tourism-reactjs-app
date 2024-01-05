d085baeb1c627c58845da13558d929235ca2bd54
suinegraj <rajabujshabani@gmail.com>
2024-01-06 02:26:22 +0300

    some changes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,36 @@
+import { Component } from "react";
 import './Navbar.css'
 import { Menu } from './Menu'
 
-const Navbar = () => {
-  return (
-    <nav className='navbar'>
-        <h1 className='logo'>ANATASOV SAFARI</h1>
-        <ul className='menu'>
-          { Menu.map((item, index)=> {
-            return (
-                <li key={ index }>
-                  <a className={ item.cName } href={ item.url }>
-                    <i className={ item.icon }></i>{ item.title }
-                  </a>
-              </li>
-            )
-          })}
-          <button>Sign up</button>
-        </ul>
-    </nav>
-  )
+
+class Navbar extends Component {
+ state= { clicked: false }
+
+ handleClick = () => {
+    this.setState( { clicked: !this.state.clicked })
+ }
+  render() {
+    return (
+        <nav className='navbar'>
+            <h1 className='logo'>ANATASOV SAFARI</h1>
+            <div className='hambuger' onClick={ this.handleClick }>
+               <i className={ this.state.clicked ? "fa fa-times" : "fa fa-bars"}></i>
+            </div>
+            <ul className='menu'>
+              { Menu.map((item, index)=> {
+                return (
+                    <li key={ index }>
+                      <a className={ item.cName } href={ item.url }>
+                        <i className={ item.icon }></i>{ item.title }
+                      </a>
+                  </li>
+                )
+              })}
+              <button>Sign up</button>
+            </ul>
+        </nav>
+      )
+  }
 }
 
-export default Navbar
+export default Navbar;
